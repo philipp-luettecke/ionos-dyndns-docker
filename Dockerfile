88c67a0b3432a978ed32dbdc3aa5be26540c6ee2
philipp-luettecke <philipp.luettecke@googlemail.com>
2024-03-24 14:33:21 +0100

    Minor updates and improvements

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:slim
 
-RUN pip3 install domain-connect-dyndns
+RUN pip3 --no-cache-dir install domain-connect-dyndns
 
 COPY --chmod=0755 entrypoint.sh /run/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,23 @@
 
 if [[ -z "${FILENAME_SETTINGS}" ]]; then
     FILENAME_SETTINGS=settings.txt
-    echo "No FILENAME_SETTINGS provided. Using default."
+    echo $(date -R) "No FILENAME_SETTINGS provided. Using default."
 fi
 
 export SETTINGS_PATH=/config/$FILENAME_SETTINGS
 
 if [[ -f $SETTINGS_PATH ]]; then
-    echo "Using $SETTINGS_PATH"
+    echo $(date -R) "Using $SETTINGS_PATH"
 else
-    echo "Settings file $SETTINGS_PATH is not found. Creating settings.txt file."
+    echo $(date -R) "Settings file $SETTINGS_PATH is not found. Creating settings.txt file."
     touch $SETTINGS_PATH
 fi
 
-echo "Everything seems to be fine now; I will update all configured domains every $UPDATE_INTERVAL minutes"
+echo $(date -R) "Everything seems to be fine now; I will update all configured domains every $UPDATE_INTERVAL minutes"
 
 while true
 do 
-    echo "Updating all configured DynDNS domains"
+    echo $(date -R) "Updating all configured DynDNS domains"
     domain-connect-dyndns update --all --config $SETTINGS_PATH
     sleep $(($UPDATE_INTERVAL*60))
 done
